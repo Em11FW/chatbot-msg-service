a7158ae124905fad3e6f8cbceb268a08aba1828d
Em11FW <fengcai.wen@usi.ch>
2022-01-24 00:57:28 +0100

    add sql file to create db

---
+++ chatbotmsg_db.sql
@@ -0,0 +1,19 @@
+drop database chatbotmsgdb;
+drop user chatbotmsg;
+create user chatbotmsg with password 'password';
+create database chatbotmsgdb with template=template0 owner=chatbotmsg;
+\connect chatbotmsgdb;
+alter default privileges grant all on tables to chatbotmsg;
+alter default privileges grant all on sequences to chatbotmsg;
+
+create table messages(
+message_id integer primary key not null,
+customer_id text not null,
+dialog_id text not null,
+message text not null,
+language text not null,
+consent boolean
+);
+
+
+create sequence messages_seq increment 1 start 1;
