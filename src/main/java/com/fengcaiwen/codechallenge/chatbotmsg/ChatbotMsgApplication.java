13a64a7707bae86e68dd8df1498dd725b07ff9ad
Em11FW <fengcai.wen@usi.ch>
2022-01-24 00:33:36 +0100

    initial commit

---
+++ src/main/java/com/fengcaiwen/codechallenge/chatbotmsg/ChatbotMsgApplication.java
@@ -0,0 +1,13 @@
+package com.fengcaiwen.codechallenge.chatbotmsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotMsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotMsgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fengcaiwen/codechallenge/chatbotmsg/ChatbotMsgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fengcaiwen.codechallenge.chatbotmsg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotMsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
